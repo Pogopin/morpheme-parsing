9552771985fe74f44630b820cf4b5449cad6dd30
Pogopin <chegarakafree@gmail.com>
2024-03-12 10:38:26 +0300

    fix parser

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,19 +1,44 @@
 
-import './assets/css/style.css';
+// import './assets/css/style.css';
+type Data = {
+    wordInformation: string;
+    content: string;
+    tableData: []
+}
+
+const contentResult = document.querySelector('.content__result');
 
 const getData = async () => {
-    const response = await fetch('http://localhost:3000/w')
+    const response = await fetch('http://localhost:3000/w', {
+        method: "POST",
+        body: encodeURIComponent('ворона')
+    })
     if(!response.ok) {
         throw new Error('Network response was not ok');
     }
-    const data = await response.json();
+    const data:Data = await response.json();
+    let template: string;
+    console.log(data)    
 
-    console.log('контент - ', typeof(data.content), data.content)
-    console.log(data.tableData)
-    return data;
+    if(!checkObject(data)) {
+        if (contentResult) {
+            template = data.content
+            contentResult.innerHTML = template;
+        }
+    } else {
+        if (contentResult) {
+            template = 'нет'
+            contentResult.innerHTML = template;
+        }
+    }    
+    // return data;
 }
-// getData();
+const checkObject = (obj: Object): boolean => {
+    return Object.keys(obj).length == 0;
+}
+getData();
 
-
-// string <span class="morphology example"><span class="stamm"><span class="wurzel">лоп<span class="red">а</span>т</span></span><span class="endung">а</span></span>
+// Разбор по составу слова: «<strong>лоп<span class="red">а</span>та</strong>» — начальная форма существительного [И.п., ед.ч.]
+// <span class="morphology example"><span class="stamm"><span class="wurzel">лоп<span class="red">а</span>т</span></span><span class="endung">а</span></span>
 // ['лопат — корень', 'а — окончание', 'лопат — основа слова']
+// <span class="morphology example"><span class="stamm"><span class="wurzel">карт<span class="red">о</span>ш</span><span class="suffix">к</span></span><span class="endung">а</span></span>
--- src/server/index.js
+++ src/server/index.js
@@ -11,19 +11,21 @@ import axios from 'axios';
 import jsdom from 'jsdom';
 
 const { JSDOM } = jsdom;
-const url = 'https://udarenieru.ru/index.php?word=on&morph_word=%D0%BB%D0%BE%D0%BF%D0%B0%D1%82%D0%B0';
-
+const url = 'https://udarenieru.ru/index.php?word=on&morph_word=';
+app.use(express.text());
 app.listen(port, () => {
     console.log(`Start app on ${port}`)
 })  
-app.get('/w', async(req, res) => {
+app.post('/w', async(req, res) => {
   try {
-    const fullData = await fetchUdarenie();     
+    const requestBody = req.body;
+    console.log(requestBody);
+    const fullData = await fetchUdarenie(requestBody);     
     res.setHeader('Access-Control-Allow-Origin', '*'); // Разрешает запросы со всех доменов
-    res.setHeader('Access-Control-Allow-Methods', 'GET'); // Разрешает методы
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Разрешает методы
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // Разрешает заголовки
     //res.setHeader('Access-Control-Allow-Credentials', true); // Разрешает cookies и другие учетные данные
-
+    
     res.send(fullData);
   } catch (err) {
       res.status(500);
@@ -31,37 +33,43 @@ app.get('/w', async(req, res) => {
   } 
 });
 
-let word = "закипело";
-let encodedWord = encodeURIComponent(word);
-console.log('код слова: ', encodedWord)
-
-const fetchUdarenie = async() => {
+const fetchUdarenie = async(wordCode) => {
   let html = null;
   try {     
-    const res = await axios.get(url)    
+    const res = await axios.get(url + `${wordCode}`)     
+    // const res = await axios.get(url + '%D0%BB%D0%BE%D0%B4%D0%BA%D0%B0')
     html = res.data;      
   } catch (error){    
       console.log(error)    
   }
 
   const dom = new JSDOM(html);
-  const document = dom.window.document;
+  const document = dom.window.document;  
+  
+  if(document.querySelector('#kuz_interpret') !== null) {  
+  
+    const firstChunk = document.querySelector('#kuz_interpret').querySelector('tbody > tr > td').innerHTML;
+    const endChunkPosition = firstChunk.indexOf('<h3');
+    const usefulData = firstChunk.slice(0, endChunkPosition - 3);
 
-//   const firstChunk = document.querySelector('#kuz_interpret').querySelector('tbody > tr > td').innerHTML
-//   const endTableTagPosition = firstChunk.indexOf('<table>')
-//   const usefulData = firstChunk.slice(0, endTableTagPosition)//удаление последней таблицы
-
-  const content = document.querySelector('#content');
-  const tableRows = document.querySelectorAll('table')[1].querySelectorAll('tbody > tr');  
-
-  const tableData = Array.from(tableRows).map((el) => {    
-    return el.textContent
-  })
-  const data = {
-    content: content.innerHTML,
-    tableData: tableData
+    const content = document.querySelector('#content');
+    const tableRows = document.querySelectorAll('table')[1].querySelectorAll('tbody > tr');  
+  
+    const tableData = Array.from(tableRows).map((el) => {    
+      return el.textContent;
+    })
+    const data = {
+      wordInformation: usefulData,
+      content: content.innerHTML,
+      tableData: tableData
+    }
+    console.log(data)
+    return JSON.stringify(data)
+  } else {
+      console.log('Такого слова нет в словаре');
+      return {}
   }
-  return JSON.stringify(data)
+
 }
 
 // fetchUdarenie()
